60e66db97d29492b9d5139087edaba294df1a054
hadoop101 <hadoop101@126.com>
2020-06-19 15:21:52 +0800

    切面=切入点（save,update,translate)+通知(start,commit,rollback,release) Spring AOP

---
+++ src/test/java/pack04/TestSpringAop.java
@@ -0,0 +1,36 @@
+package pack04;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pack01.bean.Account;
+import pack01.proxy01.AccountServiceProxy;
+import pack01.service.AccountService;
+import pack01.service.impl.AccountServiceImpl;
+import pack01.tx.MyTxManager;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
+public class TestSpringAop {
+    @Test
+    public void test01(){
+
+        ApplicationContext beanFactory = new ClassPathXmlApplicationContext("beans_aop.xml");
+
+        //3:获取service对应的
+        AccountService accountService = (AccountService) beanFactory.getBean("service");
+
+        Account account = new Account(1001L,"jack",1000D);
+        accountService.saveAccount(account);
+        accountService.updateAccount(account);
+        accountService.translate(1001L,1002L,100D);
+
+    }
+
+}
