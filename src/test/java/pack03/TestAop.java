48cca6e5c7b6dbef5cecce186b555db597c5bef5
hadoop101 <hadoop101@126.com>
2020-06-19 14:37:13 +0800

    Cglib动态代理：不修改业务类的方法的代码，增强了业务的方法

--- src/test/java/pack03/TestAop.java
+++ src/test/java/pack03/TestAop.java
@@ -1,5 +1,9 @@
 package pack03;
 
+import net.sf.cglib.proxy.Callback;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,6 +13,7 @@ import pack01.service.AccountService;
 import pack01.service.impl.AccountServiceImpl;
 import pack01.tx.MyTxManager;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -99,6 +104,52 @@ public class TestAop {
         accountService2.translate(1001L,1002L,100D);
         // System.out.println(accountService2.toString());
         //3:调用新类的方法
+
+    }
+
+    @Test
+    public void test05(){//Cglib动态的代理
+        //增强对象
+        final MyTxManager myTxManager = new MyTxManager();
+        //创建一个基于父类的新类（代理类）
+        Class clz = AccountServiceImpl.class;
+        MethodInterceptor callBack = new MethodInterceptor() {
+            //类似于 jdk动态代理里面的invoke方法，此方法内修改逻辑，影响到所有的方法
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                System.out.println("intercept 影响所有的方法");
+                //参1 o 代理对象 不要碰会形成死循环
+                //参2  代理对象的方法
+                //参3  代理对象的方法的参数
+                //参4  代理类的方法methodProxy
+                System.out.println(method);
+                System.out.println(Arrays.toString(objects));
+                System.out.println(methodProxy);
+                //判断是save,update,translate方法，添加事务逻辑，否则默认执行
+                Object returnValue = null;
+                if(method.getName().startsWith("save")||method.getName().startsWith("update")||method.getName().startsWith("translate")){
+
+                    //save update translate 需要增加事务管理的调用代理try...catch.finally
+                    System.out.println("-------------需要加事务");
+                    try{
+                        myTxManager.start();
+                        returnValue= methodProxy.invokeSuper(o,objects);
+                        myTxManager.commit();
+                    }catch (Exception exception){
+                        myTxManager.rollback();
+                    }finally {
+                        myTxManager.release();
+                    }
+                }else{
+                    //默认执行
+                    returnValue=  methodProxy.invokeSuper(o,objects);
+                }
+                return returnValue;
+            }
+        };
+        AccountServiceImpl accountService= (AccountServiceImpl) Enhancer.create(clz,callBack);//返回一个父类的子类对象（动态生成）
+        //3:调用新类的方法
+        Account account = new Account(1001L,"jack",1000D);
+        accountService.translate(1001L,1002L,100D);
 
     }
 }
